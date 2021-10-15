42041064b1b0dd538ade8ed2360e77077e93c208
Debas Gebreslasie <65129228+Debas-31@users.noreply.github.com>
2021-10-15 16:25:13 +0300

    Add filter and clearing tasks functionality

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-const { AsyncLocalStorage } = require("async_hooks");
-
 // Define UI varibles
 const form = document.querySelector('#form-task');
 const taskList = document.querySelector('.collection');
@@ -10,16 +8,19 @@ const filter = document.querySelector('#filter');
 loadEventListener();
 function loadEventListener(){
     form.addEventListener('submit', addTask);
+    taskList.addEventListener('click', removeTask);
+    clearBtn.addEventListener('click', clearTasks);
+    filter.addEventListener('keyup', filterTasks);
 }
 
 function addTask(e){
     if(taskInput.value === ''){
-        alart('Add a task');
+        alert('Add a task');
     }
     // Creating an element li
     const li = document.createElement('li');
     li.className = 'collection-item'
-    li.appendChild(document.createTextNode('taskInput.value'))
+    li.appendChild(document.createTextNode(taskInput.value))
 
     //Creating an element link
     const link = document.createElement('a');
@@ -34,7 +35,35 @@ function addTask(e){
     // Clear input task
     taskInput.value = '';
 
-    
-
     e.preventDefault();
+}
+
+function removeTask(e){
+    if(e.target.parentElement.classList.contains('delete-item')){
+        if(confirm('Are you sure?')){
+            e.target.parentElement.parentElement.remove()
+        }
+    }
+}
+
+// Function to clear task
+function clearTasks(){
+
+    while(taskList.firstChild){
+        taskList.removeChild(taskList.firstChild);
+    }
+}
+
+// Filter tasks 
+function filterTasks(e){
+    const text = e.target.value.toLowerCase();
+    console.log(text);
+    document.querySelectorAll('.collection-item').forEach(function(task){
+        const item = task.firstChild.textContent;
+        if(item.toLowerCase().indexOf(text) !== -1){
+            task.style.display = 'block';
+        }else{
+            task.style.display = 'none';
+        }
+    })
 }
