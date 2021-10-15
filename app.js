75ab87a052c58c71eeb9c7fdf13b349abe558776
Debas Gebreslasie <65129228+Debas-31@users.noreply.github.com>
2021-10-15 19:43:52 +0300

    Fix linters error

--- app.js
+++ app.js
@@ -5,65 +5,66 @@ const clearBtn = document.querySelector('.clear-tasks');
 const taskInput = document.querySelector('#task');
 const filter = document.querySelector('#filter');
 
-loadEventListener();
-function loadEventListener(){
-    form.addEventListener('submit', addTask);
-    taskList.addEventListener('click', removeTask);
-    clearBtn.addEventListener('click', clearTasks);
-    filter.addEventListener('keyup', filterTasks);
+function addTask(e) {
+  if (taskInput.value === '') {
+    // eslint-disable-next-line no-alert
+    alert('Add a task');
+  }
+  // Creating an element li
+  const li = document.createElement('li');
+  li.className = 'collection-item';
+  li.appendChild(document.createTextNode(taskInput.value));
+
+  // Creating an element link
+  const link = document.createElement('a');
+  link.className = 'delete-item secondary-content';
+
+  // Add html-icon
+  link.innerHTML = '<i class="fas fa-times"></i>';
+  li.appendChild(link);
+
+  taskList.appendChild(li);
+
+  // Clear input task
+  taskInput.value = '';
+
+  e.preventDefault();
 }
 
-function addTask(e){
-    if(taskInput.value === ''){
-        alert('Add a task');
+function removeTask(e) {
+  if (e.target.parentElement.classList.contains('delete-item')) {
+    // eslint-disable-next-line no-restricted-globals
+    if (confirm('Are you sure?')) {
+      e.target.parentElement.parentElement.remove();
     }
-    // Creating an element li
-    const li = document.createElement('li');
-    li.className = 'collection-item'
-    li.appendChild(document.createTextNode(taskInput.value))
-
-    //Creating an element link
-    const link = document.createElement('a');
-    link.className = 'delete-item secondary-content';
-
-    // Add html-icon
-    link.innerHTML = '<i class="fas fa-times"></i>';
-    li.appendChild(link);
-
-    taskList.appendChild(li);
-
-    // Clear input task
-    taskInput.value = '';
-
-    e.preventDefault();
-}
-
-function removeTask(e){
-    if(e.target.parentElement.classList.contains('delete-item')){
-        if(confirm('Are you sure?')){
-            e.target.parentElement.parentElement.remove()
-        }
-    }
+  }
 }
 
 // Function to clear task
-function clearTasks(){
+function clearTasks() {
+  while (taskList.firstChild) {
+    taskList.removeChild(taskList.firstChild);
+  }
+}
 
-    while(taskList.firstChild){
-        taskList.removeChild(taskList.firstChild);
+// Filter tasks
+function filterTasks(e) {
+  const text = e.target.value.toLowerCase();
+  document.querySelectorAll('.collection-item').forEach((task) => {
+    const item = task.firstChild.textContent;
+    if (item.toLowerCase().indexOf(text) !== -1) {
+      task.style.display = 'block';
+    } else {
+      task.style.display = 'none';
     }
+  });
 }
 
-// Filter tasks 
-function filterTasks(e){
-    const text = e.target.value.toLowerCase();
-    console.log(text);
-    document.querySelectorAll('.collection-item').forEach(function(task){
-        const item = task.firstChild.textContent;
-        if(item.toLowerCase().indexOf(text) !== -1){
-            task.style.display = 'block';
-        }else{
-            task.style.display = 'none';
-        }
-    })
+function loadEventListener() {
+  form.addEventListener('submit', addTask);
+  taskList.addEventListener('click', removeTask);
+  clearBtn.addEventListener('click', clearTasks);
+  filter.addEventListener('keyup', filterTasks);
 }
+
+loadEventListener();
